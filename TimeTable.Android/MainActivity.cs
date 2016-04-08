5efa3dfe8e4043e2e18d607d456a38647598ef18
Balazs Pekar <peba0706@gmail.com>
2016-04-08 21:46:19 +0200

    At this time the solution is really ugly, it's just pretty much experimenting all I'm doing

--- TimeTable.Android/MainActivity.cs
+++ TimeTable.Android/MainActivity.cs
@@ -1,25 +1,63 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Javax.Security.Auth;
+using TimeTable.Shared;
 
 namespace TimeTable.Android
 {
-    [Activity(Label = "Menetrend Miskolc", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.Light.NoTitleBar")]
+    [Activity(Label = "Menetrend Miskolc", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.Black")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        private static int count;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.layout1);
 
+            string _data;
+            TextView myTextView = FindViewById<TextView>(Resource.Id.textView1);
+            Button myButton = FindViewById<Button>(Resource.Id.button1);
+            myTextView.Text = "VALAMI";
+
+
+            Station station = new Station("513");
+
+            
+            station.Update();
+            _data = station.RawData;
+            //System.Threading.Thread.Sleep(3000);
+            myTextView.Text = _data;
+
+
+            myButton.Click += delegate {
+                _data = station.RawData;
+                myTextView.Text = _data;
+            };
+
+            myButton.Click += delegate
+            {
+                myButton.Text = string.Format("{0} clicks!", count++);
+                _data = station.RawData;
+                //System.Threading.Thread.Sleep(3000);
+                myTextView.Text = _data;
+            };
+
+
+        }
+
+        private void Refresh()
+        {
+            
         }
     }
 }
--- TimeTable.Android/Resources/Resource.Designer.cs
+++ TimeTable.Android/Resources/Resource.Designer.cs
@@ -60,8 +60,14 @@ namespace TimeTable.Android
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int button1 = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int textItem = 2131034112;
+			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView2 = 2131034114;
 			
 			static Id()
 			{
@@ -77,10 +83,10 @@ namespace TimeTable.Android
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int layout1 = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int MyMainListView = 2130903041;
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
